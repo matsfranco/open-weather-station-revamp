4eda2bcfd03ab68904978e1d007949b3822425d0
Mateus Franco <matsfranco@outlook.com.br>
2022-06-18 19:28:39 -0300

    DHT22 stable readings

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # open-weather-station-revamp
+
+## Features
+
+### 1. Temperature and Humidity with DHT22 Sensor
+
+This project uses the Adafruit Sensor and DHT Libs to get DHT22 Temperature, Humidity and Temperature Index data.
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,61 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+#define DHTPIN 4     // what digital pin the DHT22 is conected to
+#define DHTTYPE DHT22   // there are multiple kinds of DHT sensors
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.setTimeout(2000);
+
+  // Wait for serial to initialize.
+  while(!Serial) { }
+  
+  dht.begin();
+
+  Serial.println("Device Started");
+  Serial.println("-------------------------------------");
+  Serial.println("Running DHT!");
+  Serial.println("-------------------------------------");
+
 }
 
+int timeSinceLastRead = 0;
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  // Report every 2 seconds.
+  if(timeSinceLastRead > 2000) {
+    // Reading temperature or humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    float h = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    float t = dht.readTemperature();
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t)) {
+      Serial.println("Failed to read from DHT sensor!");
+      timeSinceLastRead = 0;
+      return;
+    }
+    
+    // Compute heat index in Celsius (isFahreheit = false)
+    float hic = dht.computeHeatIndex(t, h, false);
+
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(t);
+    Serial.print(" *C ");
+    Serial.print("Heat index: ");
+    Serial.print(hic);
+    Serial.println(" *C ");
+
+    timeSinceLastRead = 0;
+  }
+  delay(100);
+  timeSinceLastRead += 100;
 }
